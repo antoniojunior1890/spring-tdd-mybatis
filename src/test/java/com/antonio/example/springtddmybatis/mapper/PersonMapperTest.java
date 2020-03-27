60d41e775956260493458b856a3ee49c79f60f41
antoniojunior1890 <antonio.junior1890@hotmail.com>
2020-03-27 17:43:34 -0300

    Teste buscar pessoa por ddd e numero de telefone - Corrigir reiniciar sequencia database

--- src/test/java/com/antonio/example/springtddmybatis/mapper/PersonMapperTest.java
+++ src/test/java/com/antonio/example/springtddmybatis/mapper/PersonMapperTest.java
@@ -3,7 +3,6 @@ package com.antonio.example.springtddmybatis.mapper;
 
 import com.antonio.example.springtddmybatis.model.Person;
 import org.junit.jupiter.api.Test;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -14,6 +13,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest
 @TestPropertySource("classpath:application-test.yml")
 public class PersonMapperTest {
@@ -22,8 +22,28 @@ public class PersonMapperTest {
     private PersonMapper sut;
 
     @Test
-    public void mustSearchPersonByCpf() throws Exception {
+    public void mustFindPersonByCpf() throws Exception {
         Optional<Person> optional = sut.findByCpf("38767897100");
+
+        assertThat(optional.isPresent()).isTrue();
+
+        Person person = optional.get();
+        assertThat(person.getId()).isEqualTo(3L);
+        assertThat(person.getName()).isEqualTo("Cauê");
+        assertThat(person.getCpf()).isEqualTo("38767897100");
+    }
+
+    @Test
+    public void  shouldNotFindPersonCpfNonexistent() throws Exception {
+        Optional<Person> optional = sut.findByCpf("111111111");
+
+        assertThat(optional.isPresent()).isFalse();
+    }
+
+    @Test
+    public void mustFindByTelephoneDddAndNumber() throws Exception {
+        Optional<Person> optional = sut.findByTelephoneDddAndTelephoneNumber("86", "35006330");
+//        Optional<Person> optional = sut.findByTelephoneDddAndTelephoneNumber("41", "999570146");
 
         assertThat(optional.isPresent()).isTrue();
 
