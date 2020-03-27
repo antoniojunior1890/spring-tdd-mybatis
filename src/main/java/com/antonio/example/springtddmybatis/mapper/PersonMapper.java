f85b42c3bbaf631e98732a87a9855550f8da26a8
antoniojunior1890 <antonio.junior1890@hotmail.com>
2020-03-27 08:12:29 -0300

    Mudar tipo de PersonMapper

--- src/main/java/com/antonio/example/springtddmybatis/mapper/PersonMapper.java
+++ src/main/java/com/antonio/example/springtddmybatis/mapper/PersonMapper.java
@@ -3,12 +3,12 @@ package com.antonio.example.springtddmybatis.mapper;
 
 import com.antonio.example.springtddmybatis.model.Person;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public class PersonMapper {
-    public Person save(Person person) {
-        return new Person();
-    }
+public interface PersonMapper {
+
+    Person save(@Param("person") Person person);
 }
