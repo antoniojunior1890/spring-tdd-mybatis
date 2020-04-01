0baaa83551199486ff5c62ecbf7dfa956a5b0449
antoniojunior1890 <antonio.junior1890@hotmail.com>
2020-04-01 16:40:11 -0300

    Adicionar 'rest assured' para teste http rest

--- src/test/java/com/antonio/example/springtddmybatis/SpringTddMybatisApplicationTests.java
+++ src/test/java/com/antonio/example/springtddmybatis/SpringTddMybatisApplicationTests.java
@@ -1,13 +1,18 @@
 package com.antonio.example.springtddmybatis;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
-@SpringBootTest
-class SpringTddMybatisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.yml")
+public abstract class SpringTddMybatisApplicationTests {
+
+	@Value("${local.server.port}")
+	protected int port;
 
 }
---
+++ src/test/java/com/antonio/example/springtddmybatis/resource/PersonResourceTest.java
@@ -0,0 +1,6 @@
+package com.antonio.example.springtddmybatis.resource;
+
+import com.antonio.example.springtddmybatis.SpringTddMybatisApplicationTests;
+
+public class PersonResourceTest extends SpringTddMybatisApplicationTests {
+}
